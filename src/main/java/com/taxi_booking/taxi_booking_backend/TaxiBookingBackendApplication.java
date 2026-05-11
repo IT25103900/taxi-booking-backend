85dd6fe20f5bb39b83ddb178cec517da9115d318
ashinidilasha <jagodageashini.com>
2026-05-11 11:58:17 +0530

    1st commit

--- src/main/java/com/taxi_booking/taxi_booking_backend/TaxiBookingBackendApplication.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/TaxiBookingBackendApplication.java
@@ -9,6 +9,7 @@ public class TaxiBookingBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaxiBookingBackendApplication.class, args);
 		System.out.println("Hellow");
+		System.out.println("hello");
 	}
 
 }
