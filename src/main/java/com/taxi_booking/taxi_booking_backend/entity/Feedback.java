6cf1f8d3c8e1586ba16255a00f512f8f0feeb5e0
IT25103898 <it25103898@my.sliit.lk>
2026-05-12 21:11:01 +0530

    Add getters and setters to Feedback class

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Feedback.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Feedback.java
@@ -14,4 +14,40 @@ public abstract class Feedback
         this.driverId = driverId;
         this.date = date;
     }
+
+    public String getFeedbackId()
+    {
+        return feedbackId;
+    }
+    public void setFeedbackId(String feedbackId)
+    {
+        this.feedbackId = feedbackId;
+    }
+
+    public String getCustomerId()
+    {
+        return customerId;
+    }
+    public void setCustomerId(String customerId)
+    {
+        this.customerId = customerId;
+    }
+
+    public String getDriverId()
+    {
+        return driverId;
+    }
+    public void setDriverId(String driverId)
+    {
+        this.driverId = driverId;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
 }
