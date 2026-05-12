5e71590cf405b17d475334cdc1f54db62b48c576
IT25103896 <it25103896@my.sliit.lk>
2026-05-12 22:04:10 +0530

    update Van class-entity with cargo and AC attributes and methods

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
@@ -1,29 +1,46 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
 public class Van extends Vehicle {
+    private int numberOfSeats;
+    private double cargoCapacity;
+    private boolean isAirConditioned;
 
-    private int numDoors;
-    private int passengerCapacity;
-
-    public Van(String vehicleId, String brand, String model, int year, String licensePlate, String status, int numDoors, int passengerCapacity) {
-        super(vehicleId, brand, model, year, licensePlate, status);
-        this.numDoors = numDoors;
-        this.passengerCapacity = passengerCapacity;
+    public Van(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, int numberOfSeats, double cargoCapacity, boolean isAirConditioned) {
+        super(vehicleId, brand, model, pricePerKm, isAvailable);
+        this.numberOfSeats = numberOfSeats;
+        this.cargoCapacity = cargoCapacity;
+        this.isAirConditioned = isAirConditioned;
     }
 
-    public int getNumDoors() { return numDoors; }
-    public void setNumDoors(int numDoors) { this.numDoors = numDoors; }
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
 
-    public int getPassengerCapacity() { return passengerCapacity; }
-    public void setPassengerCapacity(int passengerCapacity) { this.passengerCapacity = passengerCapacity; }
+    public double getCargoCapacity() {
+        return cargoCapacity;
+    }
+    public void setCargoCapacity(double cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public boolean isAirConditioned() {
+        return isAirConditioned;
+    }
+    public void setAirConditioned(boolean airConditioned) {
+        this.isAirConditioned = airConditioned;
+    }
 
     @Override
-    public String getDetails() {
-        return super.getDetails() + ", Doors: " + numDoors + ", Capacity: " + passengerCapacity;
+    public void displayDetails() {
+        System.out.println("ID: " + vehicleId + " | Brand: " + brand + " | Model: " + model);
+        System.out.println("Seats: " + numberOfSeats + " | Cargo: " + cargoCapacity + "kg | AC: " + isAirConditioned);
     }
 
     @Override
     public double calculateMaintenanceCost() {
-        return 8500.00;
+        return 8000.00;
     }
 }
