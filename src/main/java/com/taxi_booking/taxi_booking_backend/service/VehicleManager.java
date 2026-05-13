b01e7dc909cf1b699e600273625268307d9295d3
IT25103896 <it25103896@my.sliit.lk>
2026-05-13 21:41:11 +0530

    add VehicleManager class with CRUD operations

---
+++ src/main/java/com/taxi_booking/taxi_booking_backend/service/VehicleManager.java
@@ -0,0 +1,54 @@
+package com.taxi_booking.taxi_booking_backend.service;
+
+import com.taxi_booking.taxi_booking_backend.entity.Vehicle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehicleManager {
+
+    // Store the vehicles added to the system
+    private List<Vehicle> vehicleList;
+
+    public VehicleManager() {
+        this.vehicleList = new ArrayList<>();
+    }
+
+    // Add a new vehicles to the system
+    public void addVehicle(Vehicle v) {
+        vehicleList.add(v);
+        System.out.println("Vehicle successfully added to the system.");
+    }
+
+    // View all vehicles in the system
+    public List<Vehicle> getAllVehicles() {
+        return vehicleList;
+    }
+
+    // Update vehicle details
+    public void updateVehicle(String id, Vehicle updatedVehicle) {
+        for (int i = 0; i < vehicleList.size(); i++) {
+            Vehicle currentVehicle = vehicleList.get(i);
+
+            if (currentVehicle.getVehicleId().equals(id)) {
+                vehicleList.set(i, updatedVehicle); // Replace the old vehicle with the new one
+                System.out.println("Vehicle with ID " + id + " has been successfully updated.");
+                return;
+            }
+        }
+        System.out.println("Update Failed: Vehicle with ID " + id + " not found!");
+    }
+
+    // Remove a vehicle from the system
+    public void deleteVehicle(String id) {
+        for (int i = 0; i < vehicleList.size(); i++) {
+            Vehicle currentVehicle = vehicleList.get(i);
+
+            if (currentVehicle.getVehicleId().equals(id)) {
+                vehicleList.remove(i); // Remove the vehicle from the list
+                System.out.println("Vehicle with ID " + id + " has been successfully deleted.");
+                return;
+            }
+        }
+        System.out.println("Delete Failed: Vehicle with ID " + id + " not found!");
+    }
+}
