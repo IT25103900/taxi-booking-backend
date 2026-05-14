3215b5767b30184b26bddaf156c7a0cbca3023b7
IT25103898 <it25103898@my.sliit.lk>
2026-05-14 20:48:42 +0530

    Add ArrayList and viewAllFeedbacks method

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/FeedbackManager.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/FeedbackManager.java
@@ -1,16 +1,23 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FeedbackManager
 {
 
-    private List<Feedback> feedbackList;
+    private List<Feedback> feedbackList = new ArrayList<>();
 
     // Create
     public void addFeedback(Feedback f)
     {
         feedbackList.add(f);
         System.out.println("Feedback added successfully!");
+    }
+
+    // Read
+    public void viewAllFeedbacks()
+    {
+        System.out.println("All Feedbacks: " + feedbackList);
     }
 }
