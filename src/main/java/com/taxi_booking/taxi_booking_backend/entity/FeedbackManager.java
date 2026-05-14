aacc5606e86f107c3af32da58aa6aed1e619ce02
IT25103898 <it25103898@my.sliit.lk>
2026-05-14 20:55:40 +0530

    Add updateFeedback method to FeedbackManager

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/FeedbackManager.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/FeedbackManager.java
@@ -20,4 +20,17 @@ public class FeedbackManager
     {
         System.out.println("All Feedbacks: " + feedbackList);
     }
+
+    public void updateFeedback(Feedback updatedFeedback)
+    {
+        for (int i = 0; i < feedbackList.size(); i++)
+        {
+            if (feedbackList.get(i).getFeedbackId().equals(updatedFeedback.getFeedbackId()))
+            {
+                feedbackList.set(i, updatedFeedback);
+                System.out.println("Feedback updated successfully!");
+            }
+        }
+        System.out.println("Feedback not found!");
+    }
 }
