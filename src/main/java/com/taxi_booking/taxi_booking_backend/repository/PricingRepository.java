0a1165276be5d37a24257dc377395df92e4ea9a0
IT25103899 <it25103899@my.Sliit.lk>
2026-05-16 00:10:03 +0530

    Updated PricingRepository

--- src/main/java/com/taxi_booking/taxi_booking_backend/repository/PricingRepository.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/PricingRepository.java
@@ -3,6 +3,5 @@ package com.taxi_booking.taxi_booking_backend.repository;
 import com.taxi_booking.taxi_booking_backend.entity.PricingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PricingRepository
-        extends JpaRepository<PricingModel, Long> {
+public interface PricingRepository extends JpaRepository<PricingModel, Long> {
 }
