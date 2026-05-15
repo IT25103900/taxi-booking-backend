1119830ceb171643bb72a5cb62880c2c06082460
it25103900 <it25103900@my.sliit.lk>
2026-05-15 09:22:29 +0530

    deleted unnecessary files

--- src/main/java/com/taxi_booking/taxi_booking_backend/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taxi_booking.taxi_booking_backend.repository;
-
-import com.taxi_booking.taxi_booking_backend.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
