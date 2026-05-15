1ef086336fbb7e88fabb01450eebf540e7c94740
it25103900 <it25103900@my.sliit.lk>
2026-05-15 11:58:05 +0530

    booking repository added

---
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.taxi_booking.taxi_booking_backend.repository;
+
+import com.taxi_booking.taxi_booking_backend.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByCustomerId(Long customerId);
+}
