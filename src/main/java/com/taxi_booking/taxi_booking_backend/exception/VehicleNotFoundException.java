efae13bb2ea44d6d8f7d45bc3adf04b20b1d1918
IT25103896 <it25103896@my.sliit.lk>
2026-05-15 21:09:54 +0530

    remove unused exception package and class

--- src/main/java/com/taxi_booking/taxi_booking_backend/exception/VehicleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.taxi_booking.taxi_booking_backend.exception;
-
-public class VehicleNotFoundException extends RuntimeException {
-    public VehicleNotFoundException(String message) {
-        super(message);
-    }
-}
