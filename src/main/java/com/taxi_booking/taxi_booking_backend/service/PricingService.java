84ca2f604ef1a6a0844a5be9abfe9be320402528
IT25103899 <it25103899@my.Sliit.lk>
2026-05-16 00:06:51 +0530

    Updated Pricing Service

--- src/main/java/com/taxi_booking/taxi_booking_backend/service/PricingService.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/service/PricingService.java
@@ -1,67 +1,49 @@
 package com.taxi_booking.taxi_booking_backend.service;
 
-
 import com.taxi_booking.taxi_booking_backend.dto.PricingDto;
 import com.taxi_booking.taxi_booking_backend.entity.PeakFare;
 import com.taxi_booking.taxi_booking_backend.entity.PricingModel;
 import com.taxi_booking.taxi_booking_backend.entity.StandardFare;
 import com.taxi_booking.taxi_booking_backend.repository.PricingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PricingService {
 
-    @Autowired
-    private PricingRepository pricingRepository;
+    private final PricingRepository pricingRepository;
+
+    public PricingService(PricingRepository pricingRepository) {
+        this.pricingRepository = pricingRepository;
+    }
 
     public double estimateFare(PricingDto dto) {
 
-        PricingModel pricingModel;
+        PricingModel model;
 
         if (dto.isPeakHour()) {
-
-            pricingModel = new PeakFare(
-                    200,
-                    100,
-                    1.5
-            );
-
+            model = new PeakFare(200, 100, 1.5);
         } else {
-
-            pricingModel = new StandardFare(
-                    200,
-                    100
-            );
+            model = new StandardFare(200, 100);
         }
 
-        return pricingModel.calculateTotalFare(
-                dto.getDistance(),
-                dto.getTime()
-        );
+        return model.calculateTotalFare(dto.getDistance(), dto.getTime());
     }
 
-    public PricingModel createRateTable(PricingModel pricingModel) {
-
-        return pricingRepository.save(pricingModel);
+    public PricingModel createRateTable(PricingModel model) {
+        return pricingRepository.save(model);
     }
 
-    public PricingModel updateRate(Long id, PricingModel updatedPricing) {
+    public PricingModel updateRate(Long id, PricingModel updated) {
 
-        PricingModel pricing =
-                pricingRepository.findById(id).orElseThrow();
+        PricingModel existing = pricingRepository.findById(id).orElseThrow();
 
-        pricing.setBaseRate(updatedPricing.getBaseRate());
+        existing.setBaseRate(updated.getBaseRate());
+        existing.setDistanceMultiplier(updated.getDistanceMultiplier());
 
-        pricing.setDistanceMultiplier(
-                updatedPricing.getDistanceMultiplier()
-        );
-
-        return pricingRepository.save(pricing);
+        return pricingRepository.save(existing);
     }
 
     public void deleteRate(Long id) {
-
         pricingRepository.deleteById(id);
     }
 }
