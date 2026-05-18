675a8b6c8661f879833e35ccf5a9b7b5a9bc6c7c
IT25103899 <it25103899@my.Sliit.lk>
2026-05-18 20:47:32 +0530

    Added custom pricing query method

--- src/main/java/com/taxi_booking/taxi_booking_backend/repository/PricingRepository.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/PricingRepository.java
@@ -4,4 +4,5 @@ import com.taxi_booking.taxi_booking_backend.entity.PricingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PricingRepository extends JpaRepository<PricingModel, Long> {
+    PricingModel findByBaseRate(double baseRate);
 }
