bc4e43659b13606560bdfe49bd954adaf4d8011c
IT25103897 <it25103897@my.sliit.lk>
2026-05-18 10:33:48 +0530

    Removed IDE files

---
+++ src/main/java/com/taxi_booking/taxi_booking_backend/TaxiBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taxi_booking.taxi_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiBookingBackendApplication.class, args);
+    }
+
+}
