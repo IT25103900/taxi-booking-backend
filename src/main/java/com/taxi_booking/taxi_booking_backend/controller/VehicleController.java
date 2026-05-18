0194c69294c303a0bce8652bafe09e67fd91e737
IT25103896 <it25103896@my.sliit.lk>
2026-05-18 19:48:35 +0530

    split vehicle add endpoints for Bike and Van to fix abstract deserialization

--- src/main/java/com/taxi_booking/taxi_booking_backend/controller/VehicleController.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/controller/VehicleController.java
@@ -3,6 +3,8 @@ package com.taxi_booking.taxi_booking_backend.controller;
 import com.taxi_booking.taxi_booking_backend.entity.Vehicle;
 import com.taxi_booking.taxi_booking_backend.entity.Car;
 import com.taxi_booking.taxi_booking_backend.entity.Sedan;
+import com.taxi_booking.taxi_booking_backend.entity.Bike;
+import com.taxi_booking.taxi_booking_backend.entity.Van;
 import com.taxi_booking.taxi_booking_backend.service.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ public class VehicleController {
     private VehicleManager vehicleManager;
 
     // ADD ENDPOINTS
+
     // Add a new Car
     @PostMapping("/add/car")
     public String addCar(@RequestBody Car car) {
@@ -31,13 +34,27 @@ public class VehicleController {
         return "Sedan added successfully to the system!";
     }
 
+    // Add a new Bike
+    @PostMapping("/add/bike")
+    public String addBike(@RequestBody Bike bike) {
+        vehicleManager.addVehicle(bike);
+        return "Bike added successfully to the system!";
+    }
+
+    // Add a new Van
+    @PostMapping("/add/van")
+    public String addVan(@RequestBody Van van) {
+        vehicleManager.addVehicle(van);
+        return "Van added successfully to the system!";
+    }
+
     // View all vehicles in the system
     @GetMapping("/all")
     public List<Vehicle> getAllVehicles() {
         return vehicleManager.getAllVehicles();
     }
 
-    // Update an existing vehicle (We will change this later)
+    // Update an existing vehicle (We will change this in the next commit)
     @PutMapping("/update/{id}")
     public String updateVehicle(@PathVariable String id, @RequestBody Vehicle vehicle) {
         boolean isUpdated = vehicleManager.updateVehicle(id, vehicle);
