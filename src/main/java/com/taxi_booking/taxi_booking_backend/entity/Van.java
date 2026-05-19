f12b824de31520b69f0450e3f49fbb5872de42ba
IT25103896 <it25103896@my.sliit.lk>
2026-05-19 09:34:28 +0530

    add JPA entity annotation to Van class

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
@@ -1,46 +1,35 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Van extends Vehicle {
-    private int numberOfSeats;
-    private double cargoCapacity;
-    private boolean isAirConditioned;
 
-    public Van(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, int numberOfSeats, double cargoCapacity, boolean isAirConditioned) {
+    private int payloadCapacity;
+
+    public Van(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, int payloadCapacity) {
         super(vehicleId, brand, model, pricePerKm, isAvailable);
-        this.numberOfSeats = numberOfSeats;
-        this.cargoCapacity = cargoCapacity;
-        this.isAirConditioned = isAirConditioned;
+        this.payloadCapacity = payloadCapacity;
     }
 
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
+    public Van() {
+        super();
     }
 
-    public double getCargoCapacity() {
-        return cargoCapacity;
+    public int getPayloadCapacity() {
+        return payloadCapacity;
     }
-    public void setCargoCapacity(double cargoCapacity) {
-        this.cargoCapacity = cargoCapacity;
-    }
-
-    public boolean isAirConditioned() {
-        return isAirConditioned;
-    }
-    public void setAirConditioned(boolean airConditioned) {
-        this.isAirConditioned = airConditioned;
+    public void setPayloadCapacity(int payloadCapacity) {
+        this.payloadCapacity = payloadCapacity;
     }
 
     @Override
     public void displayDetails() {
-        System.out.println("ID: " + vehicleId + " | Brand: " + brand + " | Model: " + model);
-        System.out.println("Seats: " + numberOfSeats + " | Cargo: " + cargoCapacity + "kg | AC: " + isAirConditioned);
+        System.out.println("Van Details: " + brand + " " + model + " | Payload Capacity: " + payloadCapacity + "kg");
     }
 
     @Override
     public double calculateMaintenanceCost() {
-        return 8000.00;
+        return 10000.00;
     }
 }
