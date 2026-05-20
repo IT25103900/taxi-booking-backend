c2320d27ebf0e4a5232d0abbd846f3ca6ec4af9e
IT25103898 <it25103898@my.sliit.lk>
2026-05-20 09:07:00 +0530

    Add AssessmentRepository for database operations

---
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/AssessmentRepository.java
@@ -0,0 +1,13 @@
+package com.taxi_booking.taxi_booking_backend.repository;
+
+import com.taxi_booking.taxi_booking_backend.entity.DriverAssessment;
+import com.taxi_booking.taxi_booking_backend.entity.enums.ReviewStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AssessmentRepository extends JpaRepository<DriverAssessment, Long>
+{
+    List<DriverAssessment> findByDriverIdAndStatus(Long driverId, ReviewStatus status);
+}
