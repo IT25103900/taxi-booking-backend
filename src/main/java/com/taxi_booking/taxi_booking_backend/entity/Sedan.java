e8d59b6fa28e4eb0d343d53ed3816afd08a1a390
IT25103896 <it25103896@my.sliit.lk>
2026-05-20 14:00:55 +0530

    update Sedan class with discriminator value

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Sedan.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Sedan.java
@@ -1,36 +1,13 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
-public class Sedan extends Car {
-
-    private boolean hasLuxuryInterior;
-
-    public Sedan(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, int seatingCapacity, boolean hasAC, boolean hasLuxuryInterior) {
-        super(vehicleId, brand, model, pricePerKm, isAvailable, seatingCapacity, hasAC);
-        this.hasLuxuryInterior = hasLuxuryInterior;
-    }
-
-    public Sedan() {
-        super();
-    }
-
-    public boolean isHasLuxuryInterior() {
-        return hasLuxuryInterior;
-    }
-    public void setHasLuxuryInterior(boolean hasLuxuryInterior) {
-        this.hasLuxuryInterior = hasLuxuryInterior;
-    }
-
+@DiscriminatorValue("SEDAN")
+public class Sedan extends Vehicle {
     @Override
-    public void displayDetails() {
-        super.displayDetails();
-        System.out.println("Luxury Interior: " + (hasLuxuryInterior ? "Yes" : "No"));
-    }
-
-    @Override
-    public double calculateMaintenanceCost() {
-        return 7500.00;
+    public int getPassengerCapacity() {
+        return 4;
     }
 }
