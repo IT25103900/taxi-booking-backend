0b2f7415e95d799275ee1d1b2546ee7780e37976
it25103900 <it25103900@my.sliit.lk>
2026-05-20 15:44:04 +0530

    Booking controller updated

--- src/main/java/com/taxi_booking/taxi_booking_backend/controller/BookingController.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/controller/BookingController.java
@@ -1,13 +1,11 @@
 package com.taxi_booking.taxi_booking_backend.controller;
 
 import com.taxi_booking.taxi_booking_backend.dto.BookingDto;
-import com.taxi_booking.taxi_booking_backend.entity.Booking;
-import com.taxi_booking.taxi_booking_backend.entity.enums.BookingStatus;
+import com.taxi_booking.taxi_booking_backend.entity.TaxiBooking;
 import com.taxi_booking.taxi_booking_backend.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -19,28 +17,12 @@ public class BookingController {
     private BookingService bookingService;
 
     @PostMapping("/request")
-    public ResponseEntity<Booking> requestRide(@RequestBody BookingDto bookingDto) {
-        Booking createdBooking = bookingService.requestRide(bookingDto);
-        return ResponseEntity.ok(createdBooking);
+    public ResponseEntity<TaxiBooking> create(@RequestBody BookingDto dto) {
+        return ResponseEntity.ok(bookingService.requestRide(dto));
     }
 
-    @GetMapping("/history/{customerId}")
-    public ResponseEntity<List<Booking>> viewBookingHistory(@PathVariable Long customerId) {
-        List<Booking> history = bookingService.viewBookingHistory(customerId);
-        return ResponseEntity.ok(history);
-    }
-
-    @PutMapping("/{bookingId}/status")
-    public ResponseEntity<Booking> changeRideStatus(
-            @PathVariable Long bookingId,
-            @RequestParam BookingStatus status) {
-        Booking updatedBooking = bookingService.changeRideStatus(bookingId, status);
-        return ResponseEntity.ok(updatedBooking);
-    }
-
-    @DeleteMapping("/{bookingId}/cancel-scheduled")
-    public ResponseEntity<String> deleteScheduledBooking(@PathVariable Long bookingId) {
-        bookingService.deleteScheduledBooking(bookingId);
-        return ResponseEntity.ok("Scheduled booking successfully deleted.");
+    @GetMapping("/passenger/{passengerId}")
+    public ResponseEntity<List<TaxiBooking>> getUserHistory(@PathVariable Long passengerId) {
+        return ResponseEntity.ok(bookingService.getHistory(passengerId));
     }
 }
