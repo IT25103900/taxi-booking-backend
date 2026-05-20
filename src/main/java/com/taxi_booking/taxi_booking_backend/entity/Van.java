4d2a776713dd46cfd9c7d75befaccedd22618182
IT25103896 <it25103896@my.sliit.lk>
2026-05-20 14:05:50 +0530

    update Van class with discriminator value

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Van.java
@@ -1,35 +1,13 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("VAN")
 public class Van extends Vehicle {
-
-    private int payloadCapacity;
-
-    public Van(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, int payloadCapacity) {
-        super(vehicleId, brand, model, pricePerKm, isAvailable);
-        this.payloadCapacity = payloadCapacity;
-    }
-
-    public Van() {
-        super();
-    }
-
-    public int getPayloadCapacity() {
-        return payloadCapacity;
-    }
-    public void setPayloadCapacity(int payloadCapacity) {
-        this.payloadCapacity = payloadCapacity;
-    }
-
     @Override
-    public void displayDetails() {
-        System.out.println("Van Details: " + brand + " " + model + " | Payload Capacity: " + payloadCapacity + "kg");
-    }
-
-    @Override
-    public double calculateMaintenanceCost() {
-        return 10000.00;
+    public int getPassengerCapacity() {
+        return 8;
     }
 }
