65f3a802afd9e057871ae38b5bcc4d4f3abbdd4b
IT25103896 <it25103896@my.sliit.lk>
2026-05-20 13:48:51 +0530

    update Bike class with discriminator value

--- src/main/java/com/taxi_booking/taxi_booking_backend/entity/Bike.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/entity/Bike.java
@@ -1,35 +1,13 @@
 package com.taxi_booking.taxi_booking_backend.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("BIKE")
 public class Bike extends Vehicle {
-
-    private boolean hasHelmet;
-
-    public Bike(String vehicleId, String brand, String model, double pricePerKm, boolean isAvailable, boolean hasHelmet) {
-        super(vehicleId, brand, model, pricePerKm, isAvailable);
-        this.hasHelmet = hasHelmet;
-    }
-
-    public Bike() {
-        super();
-    }
-
-    public boolean isHasHelmet() {
-        return hasHelmet;
-    }
-    public void setHasHelmet(boolean hasHelmet) {
-        this.hasHelmet = hasHelmet;
-    }
-
     @Override
-    public void displayDetails() {
-        System.out.println("Bike Details: " + brand + " " + model + " | Helmet Provided: " + hasHelmet);
-    }
-
-    @Override
-    public double calculateMaintenanceCost() {
-        return 1500.00;
+    public int getPassengerCapacity() {
+        return 1;
     }
 }
