4d4d4bd57dde7811f43df7082e7c1ce7079374db
IT25103897 <it25103897@my.sliit.lk>
2026-05-21 13:01:18 +0530

    Added PaymentRepository

--- src/main/java/com/taxi_booking/taxi_booking_backend/repository/PaymentRepository.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.taxi_booking.taxi_booking_backend.repository;
 
+import com.taxi_booking.taxi_booking_backend.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
