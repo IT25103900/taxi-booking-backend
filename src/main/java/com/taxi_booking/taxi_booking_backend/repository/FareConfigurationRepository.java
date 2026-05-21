f455001f75132547f2151034591a964efda511ac
IT25103899 <it25103899@my.Sliit.lk>
2026-05-21 12:11:55 +0530

    added FareConfigurationRepository

--- src/main/java/com/taxi_booking/taxi_booking_backend/repository/FareConfigurationRepository.java
+++ src/main/java/com/taxi_booking/taxi_booking_backend/repository/FareConfigurationRepository.java
@@ -1,4 +1,12 @@
 package com.taxi_booking.taxi_booking_backend.repository;
 
-public class FareConfigurationRepository {
+import com.taxi_booking.taxi_booking_backend.entity.FareConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface FareConfigurationRepository extends JpaRepository<FareConfiguration, Long> {
+    // Custom query to fetch rates by specific vehicle (e.g., all VAN rates)
+    List<FareConfiguration> findByVehicleType(String vehicleType);
 }
